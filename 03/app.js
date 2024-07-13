8b93f0822d4657c6e73a868b15213c9a01e5c361
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-07-13 14:14:22 +0300

    Add files via upload

--- 03/app.js
+++ 03/app.js
@@ -4,9 +4,16 @@ const http= require('http');
 /// Step 1:Create a server
 const html = fs.readFileSync('./Tameplate/index.html','utf-8')
 const server=http.createServer((request,response)=> {
-    response.end(html)
-    console.log('A new request recived')
-    //console.log(request);
+    let path=request.url;
+    if(path==='/'||path.toLocaleLowerCase()==='/home') {
+        response.end('You are in home page')
+    } else if (path.toLocaleLowerCase()=== '/about') {
+        response.end('You are in about page');
+    } else if(path.toLocaleLowerCase()==='/contact') {
+        response.end('You are in contact page');
+    } else {
+        response.end('Error 404: Page not found')
+    }
 });
 /// Step 2:Start the server
 server.listen(8000,'127.0.0.1',() => {
