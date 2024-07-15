86cc3ae626563e38a6060d23187f412b1815142a
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-07-15 12:12:18 +0300

    Add files via upload

--- 03/app.js
+++ 03/app.js
@@ -3,16 +3,37 @@ const fs = require('fs');
 const http= require('http');
 /// Step 1:Create a server
 const html = fs.readFileSync('./Tameplate/index.html','utf-8')
+let products=JSON.parse(fs.readFileSync('./Data/product.json','utf-8'));
 const server=http.createServer((request,response)=> {
     let path=request.url;
     if(path==='/'||path.toLocaleLowerCase()==='/home') {
-        response.end('You are in home page')
+        response.writeHead(200,{
+            'Content-Type':'text/html',
+            'my-header': 'Hello.world'
+        });
+        response.end(html.replace('{{%CONTENT%}}','You are in home page'))
     } else if (path.toLocaleLowerCase()=== '/about') {
-        response.end('You are in about page');
+        response.writeHead(200,{
+            'Content-Type':'text/html',
+            'my-header': 'Hello.world'
+        })
+        response.end(html.replace('{{%CONTENT%}}','You are in about page'));
     } else if(path.toLocaleLowerCase()==='/contact') {
-        response.end('You are in contact page');
+        response.writeHead(200,{
+            'Content-Type':'text/html',
+            'my-header': 'Hello.world'
+        });
+        response.end(html.replace('{{%CONTENT%}}','You are in contact page'));
+    } else if(path.toLocaleLowerCase()==='/product') {
+        response.writeHead(200,{'Content-Type':'application/json',})
+        response.end('You are in product page');
+        console.log(products);
     } else {
-        response.end('Error 404: Page not found')
+        response.writeHead(404,{
+            'Content-Type':'text/html',
+            'my-header': 'Hello.world'
+        });
+        response.end(html.replace('{{%CONTENT%}}','Error:404'))
     }
 });
 /// Step 2:Start the server
