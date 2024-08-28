02e7f12b768d6cbc2c618f4368a35322c103e9fc
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-08-29 01:55:53 +0300

    Add files via upload

---
+++ Basics/app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server=http.createServer((req,res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><tittle>Enter Massege</tittle></head>');
+        res.write('<body><form action="/message" metod="POST"><input type="text" name="message"></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><tittle>My First Page</tittle></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+server.listen(3000);
