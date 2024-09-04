2e818657425d3cb7b29b7cdf7540216b9f060904
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-09-04 12:58:54 +0300

    Add files via upload

--- Basics/APP/app.js
+++ Basics/APP/app.js
@@ -1,4 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
-const server=http.createServer(routes.handler);
-server.listen(3000);
+const express=require('express');
+
+const app=express();
+app.use('/',(req,res,next) => {
+    console.log('This always runs');
+    next();
+})
+app.use('/add-product' ,(req,res,next) => {
+    console.log("In another middlewere!");
+    res.send('<h1>Hello from product page</h1>')
+    // ....
+});
+app.use('/' ,(req,res,next) => {
+    console.log("In another middlewere!");
+    res.send('<h1>Hello from Express</h1>')
+    // ....
+});
+
+app.listen(3000);
