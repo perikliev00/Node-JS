1559242770e02335d86fd5638c5e7d49cbe1b175
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-10-06 20:19:39 +0300

    Add files via upload

--- Node.js + NoSQL/app.js
+++ Node.js + NoSQL/app.js
@@ -1,71 +1,37 @@
-const express=require('express');
+const path = require('path');
 
-const path=require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const bodyParser=require("body-parser");
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
-const errorControler = require('./controllers/error');
-const mongoConnect = require('./utill/database').mongoConnect;
+const app = express();
 
-const User=require('./models/user')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const app=express();
-
-app.set('view engine','ejs');
-
-app.set('views','views')
-
-const routesAdmin=require('./Routes/admin');
-
-const routesShop=require('./Routes/shop');
-// const { FORCE } = require('sequelize/lib/index-hints');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=> {
-    User.findById('66fc32e972b823a9a2b519e8')
+app.use((req, res, next) => {
+  User.findById('5baa2528563f16379fc8a610')
     .then(user => {
-        req.user= new User(user.name, user.email, user.cart, user._id);
-        next();
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
     })
-    .catch(err => {
-        console.log(err);
-    })
-})
+    .catch(err => console.log(err));
+});
 
-app.use(routesShop);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/admin',routesAdmin.router);
+app.use(errorController.get404);
 
-app.use(errorControler.get404);
-
-// sequelize
-//   .sync()
-//   // .sync()
-//   .then(result => {
-//     return User.findByPk(1);
-//     // console.log(result);
-//   })
-//   .then(user => {
-//     if (!user) {
-//       return User.create({ name: 'Max', email: 'test@test.com' });
-//     }
-//     return user;
-//   })
-//   .then(user => {
-//     const cart=user.createCart();
-//     return cart
-//   }) 
-//   .then(cart => {
-//     // console.log(user);
-//     app.listen(3000);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-mongoConnect( () => {
+mongoConnect(() => {
   app.listen(3000);
-
-} )
+});
